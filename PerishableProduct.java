5b26bdbad5ad9e4b6453024aa55744d19a812c26
JadenPlummx <jaden.plummer@dcmail.ca>
2026-04-05 23:52:34 -0400

    Added more comments for detail as I forgot to

--- PerishableProduct.java
+++ PerishableProduct.java
@@ -1,8 +1,7 @@
 /*
  * Name: Jaden Plummer
  * Date: 2026-04-04
- * Description: This class stores information for a perishable product
- *              and inherits from the Product class.
+ * Description: Extends Product and adds an expiry date.
  */
 
 import java.util.Date;
@@ -11,16 +10,17 @@ public class PerishableProduct extends Product {
 
     private Date expiryDate;
 
-    // Default constructor
+    // default constructor
     public PerishableProduct() {
         super();
         this.expiryDate = new Date();
     }
 
-    // Parameterized constructor
+    // constructor with expiry date
     public PerishableProduct(String sku, String productName, double unitCost, double salePrice,
                              int quantityOnHand, int quantityNeeded, String specialInstructions,
                              Date expiryDate) {
+
         super(sku, productName, unitCost, salePrice, quantityOnHand, quantityNeeded, specialInstructions);
         setExpiryDate(expiryDate);
     }
@@ -29,6 +29,7 @@ public class PerishableProduct extends Product {
         return expiryDate;
     }
 
+    // expiry date cannot be null
     public void setExpiryDate(Date expiryDate) {
         if (expiryDate == null) {
             throw new IllegalArgumentException("Expiry date cannot be null.");
@@ -36,6 +37,7 @@ public class PerishableProduct extends Product {
         this.expiryDate = expiryDate;
     }
 
+    // include expiry date in display
     @Override
     public String toString() {
         return super.toString()
